8214394c45828a642eb590aad4c9d54c23be065c
Leonid Yakovlev <leonid@cube.dev>
2021-05-18 17:12:59 +0300

    import bootstrap grid only

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
