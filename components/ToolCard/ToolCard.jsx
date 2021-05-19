abbe1f6b1a23b84917dde8e5bd539c6d3fd16824
Leonid Yakovlev <leonid@cube.dev>
2021-05-19 17:26:56 +0300

    update card

--- components/ToolCard/ToolCard.jsx
+++ components/ToolCard/ToolCard.jsx
@@ -1,4 +1,5 @@
 import styles from "./ToolCard.module.scss";
+import API from "../../api";
 
 export default function ToolCard(props) {
   return (
@@ -10,6 +11,49 @@ export default function ToolCard(props) {
           alt={props.alt || ""}
         />
         <h4 className={styles.toolCard__title}>{props.title || ""}</h4>
+      </div>
+      <p className="mt-sm mb-lg">{props.description}</p>
+      <div className="flex flex-space-between">
+        <div className="flex flex-column">
+          <span className={styles.features}>GitHub stars</span>
+          <div className={styles.github}>
+            <img src="/images/logo/github.svg" alt="github icon" />
+            <span className="features__text">
+              {API.getGitHubStarsByLink(props.link)}
+            </span>
+          </div>
+        </div>
+        <div className="flex flex-column">
+          <span className={styles.features}>Framework</span>
+          <div className={styles.framework}>
+            {props.framework && props.framework !== "Universal" ? (
+              <img
+                src={`/images/logo/${props.framework.toLowerCase()}.svg`}
+                alt={`${props.framework} icon`}
+              />
+            ) : null}
+            {/* <img src="/images/logo/github.svg" alt="github icon" /> */}
+            <span className="features__text">{props.framework}</span>
+          </div>
+        </div>
+        <div className="flex flex-column">
+          <span className={styles.features}>Language</span>
+          <div className={styles.language}>
+            {props.language ? (
+              <img
+                src={`/images/logo/${props.language.toLowerCase()}.svg`}
+                alt={`${props.language} icon`}
+              />
+            ) : null}
+            <span className="features__text">{props.language}</span>
+          </div>
+        </div>
+        <div className="flex flex-column">
+          <span className={styles.features}>Last release</span>
+          <div>
+            <span className="features__text">{API.getLastReleaseByLink()}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- pages/components.js
+++ pages/components.js
@@ -12,13 +12,25 @@ export default function Home() {
 
       <main>
         <h1>Components Page</h1>
-
         <hr></hr>
         <h3>Tool Card Component</h3>
         <h4>Props:</h4>
         <p>src - image src</p>
         <p>alt - image alt text</p>
         <p>title - tool name</p>
+        <p>framework:</p>
+        <ul>
+          <li>Universal</li>
+          <li>React</li>
+          <li>Vue</li>
+          <li>Angular</li>
+        </ul>
+        <p>language:</p>
+        <ul>
+          <li>TypeScript</li>
+          <li>JavaScript</li>
+        </ul>
+        <p>link - link on GitHub, need to load data</p>
         <p>shadow - card shadow color</p>
         <ul>
           <li>green</li>
@@ -36,6 +48,9 @@ export default function Home() {
                 title="Chart.js"
                 src="/images/logo/chartjs.svg"
                 alt="chart.js logo"
+                description="Simple yet flexible JavaScript charting for designers & developers"
+                language="JavaScript"
+                framework="Universal"
                 shadow="green"
               />
             </div>
