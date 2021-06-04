dea0d4a22a84c30cda70d65a7a6211d87b82a0ea
Leonid Yakovlev <leonid@cube.dev>
2021-06-04 14:10:56 +0300

    update v55

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$BRANCH" != "test" ]]; then
-  echo 'DEPLOY ABORTED. Please deploy to production from "master" branch only';
-  exit 1;
-fi
-
 rm -rf .cache
 rm -rf out/
 npm ci
+npm run build
 npm run export
