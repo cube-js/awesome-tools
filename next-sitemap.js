931c1f0e1e1b008b57e55db33205a5028fad655d
Leonid Yakovlev <leonid@cube.dev>
2021-06-09 15:30:13 +0300

    change sitename

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://awesome-dataviz-tools.cube.dev/",
+  siteUrl: process.env.SITE_URL || "https://awesome.cube.dev/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
