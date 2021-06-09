cff7b0bd16e73df9faf935d8bc4ba9d850f71662
Leonid Yakovlev <leonid@cube.dev>
2021-06-09 14:34:37 +0300

    change base url

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://awesome-dataviz-tools.netlify.app/",
+  siteUrl: process.env.SITE_URL || "https://awesome-dataviz-tools.cube.dev/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
