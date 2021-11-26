6713adba09996e41c2dcb165bf0adb36eb27bb58
Igor Lukanin <me@igorlukanin.io>
2021-11-27 03:20:08 +0400

    refactor: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+const withOptimizedImages = require('next-optimized-images')
 
 module.exports = withPlugins([
-  [optimizedImages, {
+  [withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+  })],
+  [withOptimizedImages, {
     images: {
       limit: -1,
     },
