e80c7c7fa9490b05dfb478def7cee9ca20881f5a
jnomeisky <javonome@gmail.com>
2023-07-25 10:20:30 -0300

    chore: fix CSP issues (#118)

--- next.config.js
+++ next.config.js
@@ -2,25 +2,43 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'report-sample' 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'report-sample' 'self' 'unsafe-inline';
+  object-src 'none';
+  base-uri 'self';
+  connect-src 'self' https://amaranth-leech.gcp-us-central1.cubecloudapp.dev https://track.cube.dev https://graphql.contentful.com;
+  font-src 'self';
+  frame-src 'self';
+  img-src 'self';
+  manifest-src 'self';
+  media-src 'self';
+  worker-src 'none';
+`;
+
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
-  headers: [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=31536000; includeSubDomains;"
-        },
-        {
-          key: "Content-Security-Policy",
-          value: "default-src 'self'; script-src 'report-sample' 'self'; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://amaranth-leech.gcp-us-central1.cubecloudapp.dev https://track.cube.dev; font-src 'self'; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; worker-src 'none';"
-        }
-      ]
-    }
-  ]
-};
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                  {
+                    key: "Strict-Transport-Security",
+                    value: "max-age=31536000; includeSubDomains;",
+                  },
+                  {
+                      key: 'Content-Security-Policy',
+                      value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+                  }
+                ]
+            },
+        ]
+    },
+}
 
 module.exports = withBundleAnalyzer(config);
